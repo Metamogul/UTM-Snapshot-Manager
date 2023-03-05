63f33555abc011a134b888b2865d0f6df7fba91a
Jan Zombik <jan.m.zombik@gmail.com>
2023-03-05 20:00:21 +0100

    Removing try-out code from App

--- UTM Snapshot-Manager/UTMSnapshotManagerApp.swift
+++ UTM Snapshot-Manager/UTMSnapshotManagerApp.swift
@@ -21,26 +21,4 @@ struct UTMSnapshotManagerApp: App {
     private func terminateApp() {
         NSApplication.shared.terminate(self)
     }
-    
-    init() {
-        let homeURL = FileManager.default.homeDirectoryForCurrentUser
-        let searchUrl = URL(string: "Documents/dev/Virtual%20Machines.localized", relativeTo: homeURL)
-        
-        var vms: [VM] = []
-        for url in FileManager.utmPackageURLsAt(searchUrl) {
-            guard let vm = VM(url: url) else {
-                continue
-            }
-            
-            vms.append(vm)
-        }
-        
-        for vm: VM in vms {
-            NSLog(vm.url.debugDescription + "\n" + vm.images.debugDescription)
-            
-            for image in vm.images {
-                NSLog(image.snapshots.debugDescription)
-            }
-        }
-    }
 }

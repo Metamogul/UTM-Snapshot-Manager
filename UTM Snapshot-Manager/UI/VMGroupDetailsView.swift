46ef51c73ad5e3c209121b59c1b7ec592239bb02
Jan Zombik <jan.m.zombik@gmail.com>
2023-03-08 19:04:03 +0100

    Making details view declaration more compact and readable

--- UTM Snapshot-Manager/UI/VMGroupDetailsView.swift
+++ UTM Snapshot-Manager/UI/VMGroupDetailsView.swift
@@ -8,41 +8,53 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
+struct Purchase: Identifiable {
+    let price: Decimal
+    let id = UUID()
+}
+
 struct VMGroupDetailsView: View {
     @Binding var vmGroup: VMGroup
+    
+    let currencyStyle = Decimal.FormatStyle.Currency(code: "USD")
+
     
     var body: some View {
         Text("vmGroup \(vmGroup.name): \(vmGroup.vms.description)")
             .padding()
-        Button("removeVM") {
-            _ = vmGroup.vms.popLast()
+        Button("removeVM", action: removeLastVM)
+        Button("addVM", action: addVMs)
+    }
+    
+    private func removeLastVM() {
+        _ = vmGroup.vms.popLast()
+    }
+    
+    private func addVMs() {
+        let utmType = UTType(filenameExtension: "utm", conformingTo: .package)
+        let openPanel = NSOpenPanel()
+        
+        openPanel.allowedContentTypes = [utmType!]
+        openPanel.allowsMultipleSelection = true
+        openPanel.canChooseDirectories = true
+        openPanel.canChooseFiles = true
+        
+        guard let urls = openPanel.runModal() == .OK ? openPanel.urls : nil else {
+            return
         }
-        Button("addVM") {
-            let utmType = UTType(filenameExtension: "utm", conformingTo: .package)
-            let openPanel = NSOpenPanel()
-            
-            openPanel.allowedContentTypes = [utmType!]
-            openPanel.allowsMultipleSelection = true
-            openPanel.canChooseDirectories = true
-            openPanel.canChooseFiles = true
-            
-            guard let urls = openPanel.runModal() == .OK ? openPanel.urls : nil else {
-                return
+        
+        let existingUrls = vmGroup.vms.map { $0.url }
+        let newVMUrls = FileManager.utmPackageURLsAt(urls)
+        for url in newVMUrls {
+            guard !existingUrls.contains(url) else {
+                continue
             }
             
-            let existingUrls = vmGroup.vms.map { $0.url }
-            let newVMUrls = FileManager.utmPackageURLsAt(urls)
-            for url in newVMUrls {
-                guard !existingUrls.contains(url) else {
-                    continue
-                }
-                
-                guard FileManager.isValidUTMPackageUrl(url) else {
-                    continue
-                }
-                
-                vmGroup.vms.append( VM(validatedUrl: url))
+            guard FileManager.isValidUTMPackageUrl(url) else {
+                continue
             }
+            
+            vmGroup.vms.append( VM(validatedUrl: url))
         }
     }
 }

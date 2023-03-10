e228692a726af52bc93a272c3487de47ad7bd7f8
Jan Zombik <jan.m.zombik@gmail.com>
2023-03-11 00:08:29 +0100

    Added placeholder description for empty VMs groups.

--- UTM Snapshot-Manager/UI/VMGroupDetailsView.swift
+++ UTM Snapshot-Manager/UI/VMGroupDetailsView.swift
@@ -18,50 +18,56 @@ struct VMGroupDetailsView: View {
     @State private var presentingShouldRemoveAlert = false
     
     var body: some View {
-        List {
-            ForEach($vmGroup.vms) { $vm in
-                VMSectionView(vmGroup: $vmGroup, vm: $vm)
+        if vmGroup.vms.isEmpty {
+            Text(LocalizedStringKey("There are no VMs in this group so far. Use the + button in the navigation area of the menu bar to add VMs."))
+                .padding()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        } else {
+            List {
+                ForEach($vmGroup.vms) { $vm in
+                    VMSectionView(vmGroup: $vmGroup, vm: $vm)
+                }
             }
+            .id(self.vmGroup)
+            .toolbar {
+                ToolbarItemGroup(placement: .principal) {
+                    Button(action: { presentingShouldRestoreAlert = true }) {
+                        Label(LocalizedStringKey("Restore latest snapshot"), systemImage: "clock.arrow.circlepath")
+                    }
+                    .help(LocalizedStringKey("Restore the latest snapshot for all images in this group"))
+                    Button(action: { presentingShouldRemoveAlert = true }) {
+                        Label(LocalizedStringKey("Remove latest snapshot"), systemImage: "minus.circle")
+                    }
+                    .help(LocalizedStringKey("Remove the latest snapshot for all images in this group"))
+                    Button(action: { presentingNewSnapshotSheet = true }) {
+                        Label(LocalizedStringKey("Create new snapshot"), systemImage: "plus.circle")
+                    }
+                    .help(LocalizedStringKey("Create a new snapshot for all images in this group"))
+                }
+                ToolbarItem(placement: .navigation) {
+                    Button(action: addVMs) {
+                        Label(LocalizedStringKey("Add more VMs"), systemImage: "plus")
+                    }
+                    .help(LocalizedStringKey("Add more VMs to this group"))
+                }
+            }
+            .navigationTitle(vmGroup.name)
+            .newSnapshotSheet(
+                presentingSheet: $presentingNewSnapshotSheet,
+                name: $newSnapshotName,
+                buttonAction: pushSnapshot
+            )
+            .snapshotManagerDialog(presentingDialog: $presentingShouldRestoreAlert,
+                                   title: LocalizedStringKey("Restore latest snapshot for all images in this group?"),
+                                   mainButtonTitle: LocalizedStringKey("Restore"),
+                                   mainButtonAction: restoreLatestSnapshot
+            )
+            .snapshotManagerDialog(presentingDialog: $presentingShouldRemoveAlert,
+                                   title: LocalizedStringKey("Remove latest snapshot for all images in this group?"),
+                                   mainButtonTitle: LocalizedStringKey("Remove"),
+                                   mainButtonRole: .destructive,
+                                   mainButtonAction: popSnapshot)
         }
-        .id(self.vmGroup)
-        .toolbar {
-            ToolbarItemGroup(placement: .principal) {
-                Button(action: { presentingShouldRestoreAlert = true }) {
-                    Label(LocalizedStringKey("Restore latest snapshot"), systemImage: "clock.arrow.circlepath")
-                }
-                .help(LocalizedStringKey("Restore the latest snapshot for all images in this group"))
-                Button(action: { presentingShouldRemoveAlert = true }) {
-                    Label(LocalizedStringKey("Remove latest snapshot"), systemImage: "minus.circle")
-                }
-                .help(LocalizedStringKey("Remove the latest snapshot for all images in this group"))
-                Button(action: { presentingNewSnapshotSheet = true }) {
-                    Label(LocalizedStringKey("Create new snapshot"), systemImage: "plus.circle")
-                }
-                .help(LocalizedStringKey("Create a new snapshot for all images in this group"))
-            }
-            ToolbarItem(placement: .navigation) {
-                Button(action: addVMs) {
-                    Label(LocalizedStringKey("Add more VMs"), systemImage: "plus")
-                }
-                .help(LocalizedStringKey("Add more VMs to this group"))
-            }
-        }
-        .navigationTitle(vmGroup.name)
-        .newSnapshotSheet(
-            presentingSheet: $presentingNewSnapshotSheet,
-            name: $newSnapshotName,
-            buttonAction: pushSnapshot
-        )
-        .snapshotManagerDialog(presentingDialog: $presentingShouldRestoreAlert,
-                               title: LocalizedStringKey("Restore latest snapshot for all images in this group?"),
-                               mainButtonTitle: LocalizedStringKey("Restore"),
-                               mainButtonAction: restoreLatestSnapshot
-        )
-        .snapshotManagerDialog(presentingDialog: $presentingShouldRemoveAlert,
-                               title: LocalizedStringKey("Remove latest snapshot for all images in this group?"),
-                               mainButtonTitle: LocalizedStringKey("Remove"),
-                               mainButtonRole: .destructive,
-                               mainButtonAction: popSnapshot)
     }
     
     private func addVMs() {
